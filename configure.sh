7d6a4020eb91809daed94eb167f3c75fe100390e
redblue-pkt <redblue-pkt@orange.pl>
2019-05-15 10:52:04 +0200

    add scripts

---
+++ configure.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source go_toolchain_helper.sh
+cd rootfs
+make defaults-7366c0
---
+++ make.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+FILE="vmlinuz-initrd-7366c0"
+TAR_IMAGE="vmlinuz-initrd_vusolo4k_$(date +%Y%m%d).tar"
+source toolchain_helper.sh
+cd rootfs
+if [ ! -e images ]; then
+	mkdir images
+fi
+#make images -j8
+cd images
+if [ -e $FILE ]; then
+	tar -cvf $TAR_IMAGE $FILE
+	gzip $TAR_IMAGE
+fi
